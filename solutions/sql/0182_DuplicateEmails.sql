006467e67ed203994d64ab8a562b59fff0132c18
Giancarlo Garbagnati <giancarlo.garbagnati@gmail.com>
2025-06-23 12:46:52 -0700

    Create 0182_DuplicateEmails.sql

---
+++ solutions/sql/0182_DuplicateEmails.sql
@@ -0,0 +1,54 @@
+/*
+182. Duplicate Emails
+Easy
+
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+---------+
+| id | email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+Output: 
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Explanation: a@b.com is repeated two times.
+*/
+
+WITH dupes AS (
+    SELECT email, count(email) as num_entries
+    FROM Person
+    GROUP BY email
+)
+SELECT DISTINCT p.email AS EMAIL
+FROM Person p
+LEFT JOIN dupes d
+ON p.email = d.email
+WHERE num_entries > 1;

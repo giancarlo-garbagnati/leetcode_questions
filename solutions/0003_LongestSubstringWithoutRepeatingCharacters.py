200bc5bc80186cc3cf876e46a3ca84d6c25ab4ed
Giancarlo Garbagnati <giancarlo.garbagnati@gmail.com>
2025-06-23 11:05:53 -0700

    Update and rename 0003.py to 0003_LongestSubstringWithoutRepeatingCharacters.py

--- solutions/0003_LongestSubstringWithoutRepeatingCharacters.py
+++ solutions/0003_LongestSubstringWithoutRepeatingCharacters.py
@@ -1,4 +1,4 @@
-"""
+""" https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 3. Longest Substring Without Repeating Characters
 Medium
 Given a string s, find the length of the longest substring without duplicate characters.
